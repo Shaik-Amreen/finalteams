cdb7aff0d02575ec934ad941d56ecc4edcc0936d
Shaik-Amreen <amreenshaik40@gmail.com>
2021-06-07 22:10:09 +0530

    Updates

---
+++ precache-manifest.50646e7f08cbe70c664e14d52ca3756c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ade33185fdc258b59c1386d78201865a",
+    "url": "/finalteams/index.html"
+  },
+  {
+    "revision": "17c12b74bf1f334ac5b2",
+    "url": "/finalteams/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "a0787930afcd14b3c233",
+    "url": "/finalteams/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "17c12b74bf1f334ac5b2",
+    "url": "/finalteams/static/js/2.40047e90.chunk.js"
+  },
+  {
+    "revision": "2f97bfe5b7c1c72b23be28739730a643",
+    "url": "/finalteams/static/js/2.40047e90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0787930afcd14b3c233",
+    "url": "/finalteams/static/js/main.bc0abce2.chunk.js"
+  },
+  {
+    "revision": "96b28f94da683a40a591",
+    "url": "/finalteams/static/js/runtime-main.bbafae60.js"
+  }
+]);
